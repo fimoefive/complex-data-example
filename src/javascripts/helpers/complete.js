a67f8f391834ae3a791f940492419d753f55749a
fimoefive <brothermj@hotmail.com>
2021-05-17 17:45:22 -0500

    Long form way to get the users on the group cards using a  Promise.all([getUsers(), getGroups(), getUserGroups(). Then filtering the spread operator and filter the ...userGroupsJoin.filter to get the ug.group_id that matches. There are three arrays used to contain the information of the objects of the groups, users, and the userGroups 'the users that are apart of each group'. All the objects from the userGroupsJoin table are pushed up into the empty array of groupRelationshipsArray, with a filter that filters an array of objects that have that group_id out of all the objects that matches the group.id. Then it is pushed into the groupRelationshipsArray. The spread operator is used to create one array of all objects with the group_id that matches. Then the groupRelationshipsArray is looped through with a .forEach to find which users are apart of that group. Then the userInfoArray empty array is declared. Then the .find is used to find the user_id out of the groupRelationshipsArray. Then the user_id is pushed into the empty userInfoArray. Then the allGroupInfoArray.push is declared to pushed up an object of all the key: 'value' pairs by spreading them. Then declared a key called users: with the value to be userInfoArray. Then the allGroupInfoArray has ten items with the group information that was spreaded inside of the new object we created. Then there is a node called users with all of the user information that belongs to that specific group. 'Long Hand Form'

--- src/javascripts/helpers/complete.js
+++ src/javascripts/helpers/complete.js
@@ -8,9 +8,25 @@ const groupsWithUsers = () => new Promise((resolve, reject) => {
   // COMPLETE THIS FUNCTION
   Promise.all([getUsers(), getGroups(), getUserGroups()])
     .then(([users, groups, userGroupsJoin]) => {
-      console.warn(users, userGroupsJoin);
+      // console.warn(users, groups, userGroupsJoin);
+      // console.warn('users', users);
       const allGroupInfoArray = [];
 
+      groups.forEach((group) => {
+        const groupRelationshipsArray = [];
+        const userInfoArray = [];
+
+        // Push all the relationships that apply to this group
+        // Use the spread to not have an array of arrays instead an array of objects
+        groupRelationshipsArray.push(...userGroupsJoin.filter((ug) => ug.group_id === group.id));
+        // console.warn(groupRelationshipsArray);
+        groupRelationshipsArray.forEach((groupRelationships) => {
+          userInfoArray.push(users.find((user) => user.id === groupRelationships.user_id));
+        });
+        // console.warn(userInfoArray);
+        allGroupInfoArray.push({ ...group, users: userInfoArray });
+      });
+      console.warn(allGroupInfoArray);
       resolve(allGroupInfoArray);
     }).catch((error) => reject(error));
 });
